1443f25bfc4e72c475ee0774e8c441e3f37ee545
Justina18 <igbanijustina@gmail.com>
2023-02-10 00:04:49 +0100

    changes

--- src/Components/Dashboard/UserPro.jsx
+++ src/Components/Dashboard/UserPro.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './User.css'
+import {CiFolderOn} from 'react-icons/ci'
+import {MdLocationOn} from 'react-icons/md'
+
 
 const UserPro = () => {
   return (
-    <div>UserPro</div>
+    <div className='UserPro'>
+        <div className='UserPro-wrap'>
+            <div className='UserPro-img-wrap'>
+                <img  className='UserPro-img' src='/profiled.png' />
+            </div>
+            <div className='UserPro-txt-wrap'>
+                <h2>Jeremy Flames</h2>
+                <button className='UserPro-doc-butt'> <CiFolderOn fontSize={20}/> Documents </button>
+               <div className='UserPro-icon-wrap'> <MdLocationOn fontSize={24} color='#006edc'/>Lagos State</div>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+        <div> 
+
+        </div>
+    </div>
   )
 }
 
