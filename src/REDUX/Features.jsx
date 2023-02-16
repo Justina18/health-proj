de0aae1fe7fefb89c8e2ae406a44cd1f6a8df2cd
Justina18 <igbanijustina@gmail.com>
2023-02-16 01:52:33 +0100

    changes

--- src/REDUX/Features.jsx
+++ src/REDUX/Features.jsx
@@ -1,24 +1,24 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = {
-  user: [],
-};
+// import { createSlice } from "@reduxjs/toolkit";
+// const initialState = {
+//   user: [],
+// };
 
-const features = createSlice({
-  name: "health",
-  initialState,
-  reducers: {
-    userData: (state, { payload }) => {
-      if (state.user.length) {
-        state.user = [{ ...payload }];
-      } else { 
-        state.user = [{ ...state.user[0], ...payload }];
-      }
-    },
-    clear_userData: (state) => {
-      state.user = [];
-    },
-  },
-});
+// const features = createSlice({
+//   name: "health",
+//   initialState,
+//   reducers: {
+//     userData: (state, { payload }) => {
+//       if (state.user.length) {
+//         state.user = [{ ...payload }];
+//       } else { 
+//         state.user = [{ ...state.user[0], ...payload }];
+//       }
+//     },
+//     clear_userData: (state) => {
+//       state.user = [];
+//     },
+//   },
+// });
 
-export const { userData,clear_userData  } = features.actions;
-export default features.reducer;
+// export const { userData,clear_userData  } = features.actions;
+// export default features.reducer;
