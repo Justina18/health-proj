33b0a5589d94fbf34de2ef3805d01065cdf3f313
Justina18 <igbanijustina@gmail.com>
2023-03-02 12:31:28 +0100

    changes

--- src/Logout.jsx
+++ src/Logout.jsx
@@ -5,6 +5,16 @@ import { UserContext } from "./UserContext";
 function Logout() {
     const [user, setUser] = useContext(UserContext);
   
+
+    const logOut = async () => {
+      const res = await axios.post(
+        `https://health360-h4ws.onrender.com/api/userlogout/:${user[0]?.data.data._id}`
+      );
+      console.log(res.data);
+      res.status === 200 ? dispatch(clearUser()) : null;
+      res.status === 200 ? navigate("/login") : null;
+    };
+
     setUser(null);
   
     return <Redirect to="/" />;
